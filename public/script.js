7d2b2f1b16bc69443356f50d7fcedf27f4cfd245
Creatorpiyush <piyushanand.1221@gmail.com>
2020-08-07 12:56:13 +0530

    peerjs

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io();
 const videoGrid = document.getElementById("video-grid");
 
 const myPeer = new Peer(undefined, {
-  host: "/",
+  host: "https://video-chat-piyush.herokuapp.com/",
   port: "5556",
 });
 
